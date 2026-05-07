1d10e9d2ea8e322cd4a94f89355de55cdb8d607a
Liminha <liminhasd1@gmail.com>
2026-05-07 20:31:32 -0300

    iniciar a venv 3.11v

--- instalar.bat
+++ instalar.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
-echo Criando ambiente virtual...
-python -m venv venv
+echo Criando ambiente virtual (Python 3.11)...
+py -3.11 -m venv venv
 echo.
 echo Instalando dependencias...
 venv\Scripts\pip install -r requirements.txt
